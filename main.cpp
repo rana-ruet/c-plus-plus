ff030e119d691bb2ff4ddd81cca926f9a111e149
Rana <rana.ruet16@gmail.com>
2023-02-02 14:31:23 +0600

    area of circle

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main(){
    cout << "Enter the radius of the circle: ";
    double radius;
    cin >> radius;
+   const double PI = 3.1416;
 
-   double area = 3.1416 * radius * radius;
+   double area = PI * radius * radius;
    cout << "Area of the circle: " << area;
 
    return 0;
