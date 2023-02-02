b8c7b411da4f6c44a9ab348f272bcc0d9b3e8525
Rana <rana.ruet16@gmail.com>
2023-02-02 17:42:13 +0600

    create a random number generator

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
+// #include <iostream>
+
+// int main(){
+//    int x = 10;
+//    double y = 5;
+//    double z = (x + 10) / (3 * y);
+
+//    std::cout << z;
+
+//    return 0;
+// }
+
+// Random Number Generator
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main(){
-   int x = 10;
-   double y = 5;
-   double z = (x + 10) / (3 * y);
+   short min = 1;
+   short max = 6;
 
-   std::cout << z;
+   srand(time(0));
 
+   int dice = (rand() % (max-min +1)) + min;
+   cout << dice;
    return 0;
 }
